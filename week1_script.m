d9900fb6dfd29f5837fb11f12d15e8894add10f5
noabeiman93-lgtm <noa.beiman93@gmail.com>
2025-12-14 08:08:50 +0200

    added anxiety variable

--- week1_script.m
+++ week1_script.m
@@ -6,7 +6,8 @@ subject_number = [1; 2; 3; 4; 5; 6];
 gender = ["M"; "M"; "M"; "F"; "F"; "F"];
 age = [15; 17; 18; 20; 16; 30];
 depression = [0; 1; 0; 1; 0; 1];
+anxiety = [0; 1; 1; 1; 0; 1];
 
-t = table(subject_number, age, gender, depression);
+t = table(subject_number, age, gender, depression, anxiety);
 
 writetable(t, 't.csv');
